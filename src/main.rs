947ab1c6ed051342ab5286a8d65026aa1cedb251
Kevin Hamacher <kevin.hamacher@ruhr-uni-bochum.de>
2016-03-23 23:19:27 +0100

    Added src

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+extern crate rustc_serialize;
+use rustc_serialize::json;
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct Command {
+	cid: u64,
+	uid: u32,
+	target: String,
+}
+
+fn get_string(cmd: &Command) -> String {
+	return json::encode(&cmd).unwrap();
+}
+
+fn main(){
+	let bs = Command{
+		cid: 0,
+		uid: 1,
+		target: "something".to_string()
+	};
+	println!("So guess what?: {}", get_string(&bs));
+}
+
+#[test]
+fn some_test() {
+	assert_eq!(
+		get_string(
+			&Command{cid: 0, uid: 1, target: "cat".to_string()}
+		), "{\"cid\":0,\"uid\":1,\"target\":\"cat\"}");
+}
+
+#[test]
+fn some_fail() {
+	assert!(false);
+}
